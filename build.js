6d6c86675df315f326b4ea89f32faf35e7e963cd
DilmurodMuxtorov <dilmurodsenator@gmail.com>
2024-01-04 20:32:25 +0500

    push

--- build.js
+++ build.js
@@ -35031,22 +35031,22 @@
   },
 ]);
 
-// document.oncontextmenu = cmenu;
-// function cmenu() {
-//   return false;
-// }
+document.oncontextmenu = cmenu;
+function cmenu() {
+  return false;
+}
 
-// document.onkeydown = function (e) {
-//   if (event.keyCode == 123) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-//     return false;
-//   }
-// };
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
